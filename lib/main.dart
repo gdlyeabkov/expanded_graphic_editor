152cc3d29f5e4b0621bd9320dfa7372ca8f69647
gdlyeabkov <gdlyeabkov@gmail.com>
2022-03-17 18:56:06 +0300

    add_draw_curves_without_polygone

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:graphiceditor/softtrack_canvas.dart';
 import 'package:touchable/touchable.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,6 +36,217 @@ class _MyHomePageState extends State<MyHomePage> {
 
   double touchX = 0;
   double touchY = 0;
+  List shapes = [];
+  List<Offset> points = [];
+  // int pointsCursor = -1;
+  String curve = 'rect';
+  String shape = 'rect';
+  Color paleteColor = Colors.black;
+  String activeTool = 'pen';
+  String textToolDialogContent = '';
+
+  onTouchStart(event, context) {
+    setState(() {
+      touchX = event.globalPosition.dx;
+      touchY = event.globalPosition.dy;
+      if (activeTool == 'curve') {
+        if (curve == 'polygone') {
+          shapes.add({
+            'points': points,
+            'type': 'curve',
+            'curveType': curve,
+          });
+        } else {
+          shapes.add({
+            'x1': touchX,
+            'y1': touchY,
+            'x2': touchX,
+            'y2': touchY,
+            'color': paleteColor,
+            'type': 'curve',
+            'curveType': curve,
+          });
+        }
+      } else if (activeTool == 'shape') {
+        if (shape == 'polygone') {
+
+        } else {
+          shapes.add({
+            'x1': touchX,
+            'y1': touchY,
+            'x2': touchX,
+            'y2': touchY,
+            'color': paleteColor,
+            'type': 'shape',
+            'shapeType': shape,
+          });
+        }
+      }
+    });
+  }
+
+  onTouchMove(event, context) {
+    print('DragUpdateEvent ${event.globalPosition.dx} ${event.globalPosition.dy}');
+    setState(() {
+      touchX = event.globalPosition.dx;
+      touchY = event.globalPosition.dy;
+      if (activeTool == 'pen') {
+        shapes.add({
+          'x1': touchX,
+          'y1': touchY,
+          'x2': touchX + 1,
+          'y2': touchY + 1,
+          'color': paleteColor,
+          'type': 'line'
+        });
+      } else if (activeTool == 'eraser') {
+        shapes.add({
+          'x1': touchX,
+          'y1': touchY,
+          'x2': touchX + 50,
+          'y2': touchY + 50,
+          'type': 'eraser'
+        });
+      } else if (activeTool == 'curve') {
+        shapes[shapes.length - 1]['x2'] = touchX;
+        shapes[shapes.length - 1]['y2'] = touchY;
+      } else if (activeTool == 'shape') {
+        shapes[shapes.length - 1]['x2'] = touchX;
+        shapes[shapes.length - 1]['y2'] = touchY;
+      }
+    });
+  }
+
+  onTouchEnd(event, context) {
+    setState(() {
+
+      if (activeTool == 'curve') {
+        if (curve == 'polygone') {
+          if (points.length >= 3) {
+            points = [];
+          }
+          if (points.length <= 0) {
+            points = [
+              Offset(
+                touchX,
+                touchY
+              )
+            ];
+            shapes.add({
+              'points': points,
+              'type': 'curve',
+              'curveType': curve
+            });
+          } else {
+            shapes[shapes.length - 1]['points'].add(
+              Offset(
+                touchX,
+                touchY
+              )
+            );
+            points.add(
+              Offset(
+                touchX,
+                touchY
+              )
+            );
+          }
+        }
+      } else if (activeTool == 'fill') {
+        shapes.add({
+          'type': 'fill'
+        });
+      } else if (activeTool == 'gradient') {
+        shapes.add({
+          'type': 'gradient'
+        });
+      } else if (activeTool == 'text') {
+        showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text(''),
+            content: SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Текст'
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 200,
+                          child: TextField(
+                            onChanged: (value) {
+                              textToolDialogContent = value;
+                            }
+                          )
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Fluttertoast.showToast(
+                              msg: 'В настоящее время фукнция\nголосового набора\nне используется',
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.black,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                            );
+                          },
+                          child: Icon(
+                            Icons.mic
+                          )
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                          },
+                          child:  Icon(
+                            Icons.keyboard
+                          )
+                        ),
+                      ]
+                    )
+                  ]
+                )
+              )
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    print('добавляю текст');
+                    shapes.add({
+                      'x': touchX,
+                      'y': touchY,
+                      'content': textToolDialogContent,
+                      'type': 'text'
+                    });
+                    textToolDialogContent = '';
+                  });
+                  return Navigator.pop(context, 'OK');
+                },
+                child: const Text(
+                  'Задать'
+                )
+              ),
+              TextButton(
+                onPressed: () {
+                  return Navigator.pop(context, 'OK');
+                },
+                child: const Text(
+                  'Отмена'
+                )
+              )
+            ]
+          )
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,43 +258,331 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-
-            ]
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 15
+              ),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeTool = 'pen';
+                      });
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.brush
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          activeTool = 'eraser';
+                        });
+                      },
+                      child: Container(
+                      child: Icon(
+                        Icons.crop_square
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeTool = 'curve';
+                      });
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.crop_square
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeTool = 'shape';
+                      });
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.rectangle
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeTool = 'fill';
+                      });
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.format_color_fill
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeTool = 'gradient';
+                      });
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.gradient
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activeTool = 'text';
+                      });
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.text_fields
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    )
+                  )
+                ]
+              )
+            ),
+            scrollDirection: Axis.horizontal,
           ),
           Row(
-            children: [
+            children: (
+              (activeTool == 'pen' || activeTool == 'eraser') ?
+                [
+                  TextButton(
+                    child: Text(
+                      'Коррекция 0'
+                    ),
+                    onPressed: () {
 
-            ]
+                    }
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Привязка'
+                      ),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            child: Icon(
+                              Icons.grid_3x3
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.grid_3x3
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.grid_3x3
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.grid_3x3
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.grid_3x3
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.grid_3x3
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                          GestureDetector(
+                            child: Icon(
+                              Icons.more_vert
+                            ),
+                            onTap: () {
+
+                            }
+                          ),
+                        ]
+                      )
+                    ]
+                  )
+                ]
+              : (activeTool == 'curve') ?
+                [
+                  GestureDetector(
+                    child: Container(
+                      child: Icon(
+                        Icons.crop_square
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        curve = 'rect';
+                      });
+                    }
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      child: Icon(
+                        Icons.circle_outlined
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        curve = 'oval';
+                      });
+                    }
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      child: Icon(
+                        Icons.pentagon_outlined
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        curve = 'polygone';
+                      });
+                    }
+                  )
+                ]
+              : (activeTool == 'shape') ?
+                [
+                  GestureDetector(
+                    child: Container(
+                      child: Icon(
+                        Icons.rectangle
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        shape = 'rect';
+                      });
+                    }
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      child: Icon(
+                        Icons.circle
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        shape = 'oval';
+                      });
+                    }
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      child: Icon(
+                        Icons.pentagon
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        shape = 'polygone';
+                      });
+                    }
+                  )
+                ]
+              :
+                [
+
+                ]
+            )
           ),
-          // CanvasTouchDetector(
-          //   builder: (BuildContext context) => CustomPaint(
-          //     painter: SofttrackCanvas(context)
-          //   ),
-          // ),
           GestureDetector(
             child: Container(
               child: CustomPaint(
-                painter: SofttrackCanvas(context, touchX, touchY)
+                painter: SofttrackCanvas(context, touchX, touchY, shapes)
               ),
               width: 415,
-              height: 750
+              height: 650
             ),
+            onHorizontalDragStart: (event) {
+              onTouchStart(event, context);
+            },
+            onVerticalDragStart: (event) {
+              onTouchStart(event, context);
+            },
             onHorizontalDragUpdate: (event) {
-              print('onHorizontalDragUpdateEvent ${event.globalPosition.dx} ${event.globalPosition.dy}');
-              setState(() {
-                touchX = event.globalPosition.dx;
-                touchY = event.globalPosition.dy;
-              });
+              onTouchMove(event, context);
             },
             onVerticalDragUpdate: (event) {
-              print('onVerticalDragUpdateEvent ${event.globalPosition.dx} ${event.globalPosition.dy}');
-              setState(() {
-                touchX = event.globalPosition.dx;
-                touchY = event.globalPosition.dy;
-              });
-            }
+              onTouchMove(event, context);
+            },
+            onHorizontalDragEnd: (event) {
+              onTouchEnd(event, context);
+            },
+            onVerticalDragEnd: (event) {
+              onTouchEnd(event, context);
+            },
           ),
           Row(
             children: [
@@ -103,6 +603,15 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               Text(
                 'Палитра'
+              ),
+              ColorPicker(
+                pickerColor: paleteColor,
+                onColorChanged: (Color color) {
+                  setState(() {
+                    paleteColor = color;
+                  });
+                },
+                pickerAreaBorderRadius: BorderRadius.circular(1000.0),
               )
             ]
           )
--- lib/softtrack_canvas.dart
+++ lib/softtrack_canvas.dart
@@ -1,4 +1,5 @@
 // import 'dart:html';
+import 'dart:ui' as ui;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,20 +10,114 @@ class SofttrackCanvas extends CustomPainter {
   late BuildContext context;
   double touchX = 0;
   double touchY = 0;
+  List shapes = [];
 
-  SofttrackCanvas(context, touchX, touchY) {
+  SofttrackCanvas(context, touchX, touchY, shapes) {
     this.context = context;
     this.touchX = touchX;
     this.touchY = touchY;
+    this.shapes = shapes;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    var myCanvas = TouchyCanvas(context, canvas);
-    Paint paint = Paint();
-    paint.color = Colors.transparent;
-    paint.color = Colors.red;
-    canvas.drawLine(Offset(0, 0), Offset(touchX, touchY), Paint());
+
+    for (var shape in shapes) {
+      String type = shape['type'];
+      if (type == 'line') {
+        double x1 = shape['x1'];
+        double y1 = shape['y1'];
+        double x2 = shape['x2'];
+        double y2 = shape['y2'];
+        Color color  = shape['color'];
+        Paint linePaint = Paint();
+        linePaint.color = color;
+        canvas.drawLine(Offset(x1, y1), Offset(x2, y2), linePaint);
+      } else if (type == 'eraser') {
+        Paint eraserPaint = Paint();
+        eraserPaint.color = Colors.white;
+        double x1 = shape['x1'];
+        double y1 = shape['y1'];
+        double x2 = shape['x2'];
+        double y2 = shape['y2'];
+        canvas.drawLine(Offset(x1, y1), Offset(x2, y2), eraserPaint);
+      } else if (type == 'curve') {
+        Paint curvePaint = Paint();
+        curvePaint.style = PaintingStyle.stroke;
+        curvePaint.strokeWidth = 1.0;
+        String curveType = shape['curveType'];
+        if (curveType == 'polygone') {
+          Path path = Path();
+          // List<Offset> points = [
+          //   Offset(100, 500),
+          //   Offset(200, 600),
+          //   Offset(300, 400),
+          // ];
+          List<Offset> points = shape['points'];
+          print('points: ${points.length}');
+          path.addPolygon(points, true);
+          canvas.drawPath(path, curvePaint);
+        } else {
+          double x1 = shape['x1'];
+          double y1 = shape['y1'];
+          double x2 = shape['x2'];
+          double y2 = shape['y2'];
+          Color color  = shape['color'];
+          curvePaint.color = color;
+          if (curveType == 'rect') {
+            canvas.drawRect(Rect.fromPoints(Offset(x1, y1), Offset(x2, y2)), curvePaint);
+          } else if (curveType == 'oval') {
+            canvas.drawOval(Rect.fromPoints(Offset(x1, y1), Offset(x2, y2)), curvePaint);
+          }
+        }
+      } else if (type == 'shape') {
+        String shapeType = shape['shapeType'];
+        Paint shapePaint = Paint();
+        double x1 = shape['x1'];
+        double y1 = shape['y1'];
+        double x2 = shape['x2'];
+        double y2 = shape['y2'];
+        Color color  = shape['color'];
+        shapePaint.color = color;
+        if (shapeType == 'polygone') {
+
+        } else {
+          if (shapeType == 'rect') {
+            canvas.drawRect(Rect.fromPoints(Offset(x1, y1), Offset(x2, y2)), shapePaint);
+          } else if (shapeType == 'oval') {
+            canvas.drawOval(Rect.fromPoints(Offset(x1, y1), Offset(x2, y2)), shapePaint);
+          }
+        }
+      } else if (type == 'fill') {
+        Paint fillPaint = Paint();
+        fillPaint.color = Colors.red;
+        canvas.drawRect(Rect.fromPoints(Offset(0, 0), Offset(1000, 1000)), fillPaint);
+      } else if (type == 'gradient') {
+        Paint gradientPaint = Paint();
+        gradientPaint.shader = ui.Gradient.linear(Offset(0, 0), Offset(415, 715), [Colors.black, Colors.white]);
+        canvas.drawRect(Rect.fromPoints(Offset(0, 0), Offset(415, 715)), gradientPaint);
+      } else if (type == 'text') {
+        double x = shape['x'];
+        double y = shape['y'];
+        String content = shape['content'];
+        print('text: ${content}');
+        TextPainter textPainter = TextPainter(
+          textDirection: TextDirection.ltr,
+          text: TextSpan(
+            text: '$content',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            )
+          )
+        );
+        textPainter.layout(
+          minWidth: 0,
+          maxWidth: double.maxFinite,
+        );
+        textPainter.paint(canvas, Offset(x, y));
+      }
+    }
   }
 
   @override
